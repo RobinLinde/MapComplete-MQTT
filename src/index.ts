8591cf023d4094bb760dda6b25eade9e45a2660d
Robin van der Linde <robin.vanderlinde@gmail.com>
2023-05-08 01:05:45 +0200

    🐛 Add logging, catch error

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import * as dotenv from "dotenv";
 import { AsyncMqttClient, connectAsync } from "async-mqtt";
 import type { APIResponse, Changeset } from "./@types/osmcha";
+import winston = require("winston");
 
 // Standard variables
 dotenv.config();
@@ -11,6 +12,25 @@ const mqtt_port = process.env.MQTT_PORT
 const mqtt_username = process.env.MQTT_USERNAME || "";
 const mqtt_password = process.env.MQTT_PASSWORD || "";
 const dry_run = process.env.DRY_RUN === "True" || false;
+
+// Create a logger
+const logger = winston.createLogger({
+  transports: [
+    new winston.transports.Console({
+      format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.printf(
+          (info) => `${info.timestamp} ${info.level}: ${info.message}`
+        ),
+        winston.format.colorize({ all: true })
+      ),
+    }),
+    new winston.transports.File({
+      filename: "mapcomplete-stats.log",
+    }),
+  ],
+  level: "info",
+});
 
 // Configure timezone
 process.env.TZ = "Europe/London";
@@ -40,7 +60,7 @@ let mapCompleteChangesets: Changeset[] = [];
 
 // Check if the OSMCha token is set
 if (process.env.OSMCHA_TOKEN === undefined) {
-  console.error("OSMCHA_TOKEN is not set");
+  logger.error("OSMCHA_TOKEN is not set");
   process.exit(1);
 }
 
@@ -99,10 +119,10 @@ function findTop(item: Record<string, number>): string {
  * @param client The MQTT client to publish the data to
  */
 async function update(client: AsyncMqttClient) {
-  console.log("Performing update");
+  logger.info("Performing update");
   // Check if the last update time is still today
   if (new Date(lastUpdateTime).getDate() !== new Date().getDate()) {
-    console.log("New day, resetting changesets");
+    logger.info("New day, resetting changesets");
     // Reset the mapCompleteChangesets array
     mapCompleteChangesets = [];
     // Reset the lastUpdateTime
@@ -115,273 +135,283 @@ async function update(client: AsyncMqttClient) {
     .slice(0, 19)
     .replace("T", " ");
 
-  // Get the changesets from OSMCha
-  const response = await fetch(
-    `https://osmcha.org/api/v1/changesets/?date__gte=${encodeURIComponent(
-      date
-    )}&editor=MapComplete`,
-    {
-      headers: {
-        accept: "application/json",
-        Authorization: process.env.OSMCHA_TOKEN,
-      },
+  // Wrap the update in a try/catch block, so we can catch errors
+  try {
+    // Get the changesets from OSMCha
+    const response = await fetch(
+      `https://osmcha.org/api/v1/changesets/?date__gte=${encodeURIComponent(
+        date
+      )}&editor=MapComplete`,
+      {
+        headers: {
+          accept: "application/json",
+          Authorization: process.env.OSMCHA_TOKEN,
+        },
+      }
+    );
+
+    // Parse the response
+    const data: APIResponse = await response.json();
+
+    logger.info(`Found ${data.features.length} new changesets`);
+
+    // Loop through the changesets
+    for (const changeset of data.features) {
+      // Check if the changeset is already in the array
+      if (mapCompleteChangesets.find((c) => c.id === changeset.id)) {
+        // Skip this changeset
+        continue;
+      }
+
+      // Add the changeset to the array
+      mapCompleteChangesets.push(changeset);
     }
-  );
 
-  // Parse the response
-  const data: APIResponse = await response.json();
+    // Sort the changesets by ID
+    mapCompleteChangesets.sort((a, b) => a.id - b.id);
 
-  console.log(`Found ${data.features.length} new changesets`);
+    // Determine all statistics
 
-  // Loop through the changesets
-  for (const changeset of data.features) {
-    // Check if the changeset is already in the array
-    if (mapCompleteChangesets.find((c) => c.id === changeset.id)) {
-      // Skip this changeset
-      continue;
-    }
+    // Total number of changesets
+    const total = mapCompleteChangesets.length;
 
-    // Add the changeset to the array
-    mapCompleteChangesets.push(changeset);
-  }
+    // Unique users
+    const userCount = new Set(
+      mapCompleteChangesets.map((c) => c.properties.uid)
+    ).size;
 
-  // Sort the changesets by ID
-  mapCompleteChangesets.sort((a, b) => a.id - b.id);
-
-  // Determine all statistics
-
-  // Total number of changesets
-  const total = mapCompleteChangesets.length;
-
-  // Unique users
-  const userCount = new Set(mapCompleteChangesets.map((c) => c.properties.uid))
-    .size;
-
-  // Users
-  let users = mapCompleteChangesets.reduce((acc, cur) => {
-    // If the user is not in the object, add it
-    if (acc[cur.properties.user] === undefined) {
-      acc[cur.properties.user] = 0;
-    }
-    // Increase the count for the user
-    acc[cur.properties.user]++;
-    return acc;
-  }, {} as Record<string, number>);
-  // Sort the users by the number of changesets
-  users = Object.fromEntries(
-    Object.entries(users).sort(([, a], [, b]) => b - a)
-  );
-
-  // Unique themes
-  const themeCount = new Set(
-    mapCompleteChangesets.map((c) => c.properties.metadata["theme"])
-  ).size;
-
-  // Themes
-  let themes = mapCompleteChangesets.reduce((acc, cur) => {
-    // Get the theme from the changeset
-    const theme = cur.properties.metadata["theme"];
-    // If the theme is not in the object, add it
-    if (acc[theme] === undefined) {
-      acc[theme] = 0;
-    }
-    // Increase the count for the theme
-    acc[theme]++;
-    return acc;
-  }, {} as Record<string, number>);
-  // Sort the themes by the number of changesets
-  themes = Object.fromEntries(
-    Object.entries(themes).sort(([, a], [, b]) => b - a)
-  );
-
-  // Make a list of colors for each changeset
-  const colors = mapCompleteChangesets.map((c) => {
-    // Get the theme from the changeset
-    const theme = c.properties.metadata["theme"];
-    // Check if the theme is in the lookup table
-    if (themeColors[theme] !== undefined) {
-      // Return the color from the lookup table
-      return themeColors[theme];
-    }
-    // Return the default color
-    return themeColors.default;
-  });
-
-  // Total number of answered questions of all changesets
-  const questions = mapCompleteChangesets.reduce((acc, cur) => {
-    // Check if the changeset has the answer metadata
-    if (cur.properties.metadata["answer"] === undefined) {
-      // Skip this changeset
+    // Users
+    let users = mapCompleteChangesets.reduce((acc, cur) => {
+      // If the user is not in the object, add it
+      if (acc[cur.properties.user] === undefined) {
+        acc[cur.properties.user] = 0;
+      }
+      // Increase the count for the user
+      acc[cur.properties.user]++;
       return acc;
-    }
-    // Get the number of answered questions from the changeset
-    const changesetQuestions = parseInt(cur.properties.metadata["answer"]);
-    // Add the number of answered questions to the total
-    return acc + changesetQuestions;
-  }, 0);
+    }, {} as Record<string, number>);
+    // Sort the users by the number of changesets
+    users = Object.fromEntries(
+      Object.entries(users).sort(([, a], [, b]) => b - a)
+    );
 
-  // Total number of added images of all changesets
-  const images = mapCompleteChangesets.reduce((acc, cur) => {
-    // Check if the changeset has the image metadata
-    if (cur.properties.metadata["add-image"] === undefined) {
-      // Skip this changeset
+    // Unique themes
+    const themeCount = new Set(
+      mapCompleteChangesets.map((c) => c.properties.metadata["theme"])
+    ).size;
+
+    // Themes
+    let themes = mapCompleteChangesets.reduce((acc, cur) => {
+      // Get the theme from the changeset
+      const theme = cur.properties.metadata["theme"];
+      // If the theme is not in the object, add it
+      if (acc[theme] === undefined) {
+        acc[theme] = 0;
+      }
+      // Increase the count for the theme
+      acc[theme]++;
       return acc;
-    }
-    // Get the number of added images from the changeset
-    const changesetImages = parseInt(cur.properties.metadata["add-image"]);
-    // Add the number of added images to the total
-    return acc + changesetImages;
-  }, 0);
+    }, {} as Record<string, number>);
+    // Sort the themes by the number of changesets
+    themes = Object.fromEntries(
+      Object.entries(themes).sort(([, a], [, b]) => b - a)
+    );
 
-  // Total number of added points of all changesets
-  const points = mapCompleteChangesets.reduce((acc, cur) => {
-    // Check if the changeset has the create metadata
-    if (cur.properties.metadata["create"] === undefined) {
-      // Skip this changeset
-      return acc;
-    }
-    // Get the number of added points from the changeset
-    const changesetPoints = parseInt(cur.properties.metadata["create"]);
-    // Add the number of added points to the total
-    return acc + changesetPoints;
-  }, 0);
-
-  let lastId: number | null;
-  let lastUser: string | null;
-  let lastTheme: string | null;
-  let lastColor: string;
-  let lastColorRgb: [number, number, number] | null;
-
-  // Check if we actually have any changesets before we try to get the last changeset
-  if (mapCompleteChangesets.length > 0) {
-    // Get the last changeset
-    const lastChangeset =
-      mapCompleteChangesets[mapCompleteChangesets.length - 1];
-    // Get the last changeset ID
-    lastId = lastChangeset.id;
-    // Get the last changeset user
-    lastUser = lastChangeset.properties.user;
-    // Get the last changeset theme
-    lastTheme = lastChangeset.properties.metadata["theme"];
-    // Get the last changeset color
-    lastColor = colors[colors.length - 1];
-    lastColorRgb = hexToRgb(lastColor);
-  } else {
-    // Set the last changeset ID to null
-    lastId = null;
-    // Set the last changeset user to null
-    lastUser = null;
-    // Set the last changeset theme to null
-    lastTheme = null;
-    // Set the last changeset color to null
-    lastColor = null;
-    lastColorRgb = null;
-  }
-
-  const statistics = {
-    changesets: {
-      total,
-      last: lastId,
-      lastColor,
-      lastColorRgb,
-      colors,
-      colorsStr: colors.join(","),
-      colorsRgb: colors.map((c) => hexToRgb(c)),
-      colorsRgbStr: colors.map((c) => hexToRgb(c)).join(","),
-    },
-    users: {
-      total: userCount,
-      top: findTop(users),
-      last: lastUser,
-      users,
-    },
-    themes: {
-      total: themeCount,
-      top: findTop(themes),
-      last: lastTheme,
-      themes,
-    },
-    questions,
-    images,
-    points,
-  };
-
-  // Log the statistics
-  console.log(
-    `Total changesets for today: ${statistics.changesets.total} by ${statistics.users.total} users, using ${statistics.themes.total} different themes`
-  );
-  console.log(
-    `Top 5 users: ${Object.entries(statistics.users.users)
-      .slice(0, 5)
-      .map(([user, count]) => `${user} (${count})`)
-      .join(", ")}`
-  );
-  console.log(
-    `Top 5 themes: ${Object.entries(statistics.themes.themes)
-      .slice(0, 5)
-      .map(([theme, count]) => `${theme} (${count})`)
-      .join(", ")}`
-  );
-  console.log(
-    `Total questions answered: ${statistics.questions}, total images added: ${statistics.images}, total points added: ${statistics.points}`
-  );
-
-  if (!dry_run) {
-    // Publish the statistics to MQTT
-    console.log("Publishing statistics to MQTT");
-    await client.publish("mapcomplete/statistics", JSON.stringify(statistics), {
-      retain: true,
+    // Make a list of colors for each changeset
+    const colors = mapCompleteChangesets.map((c) => {
+      // Get the theme from the changeset
+      const theme = c.properties.metadata["theme"];
+      // Check if the theme is in the lookup table
+      if (themeColors[theme] !== undefined) {
+        // Return the color from the lookup table
+        return themeColors[theme];
+      }
+      // Return the default color
+      return themeColors.default;
     });
 
-    // Also publish everything to its own topic
-    for (const [key, value] of Object.entries(statistics)) {
-      // Handle nested objects
-      if (typeof value === "object") {
-        for (const [subKey, subValue] of Object.entries(value)) {
+    // Total number of answered questions of all changesets
+    const questions = mapCompleteChangesets.reduce((acc, cur) => {
+      // Check if the changeset has the answer metadata
+      if (cur.properties.metadata["answer"] === undefined) {
+        // Skip this changeset
+        return acc;
+      }
+      // Get the number of answered questions from the changeset
+      const changesetQuestions = parseInt(cur.properties.metadata["answer"]);
+      // Add the number of answered questions to the total
+      return acc + changesetQuestions;
+    }, 0);
+
+    // Total number of added images of all changesets
+    const images = mapCompleteChangesets.reduce((acc, cur) => {
+      // Check if the changeset has the image metadata
+      if (cur.properties.metadata["add-image"] === undefined) {
+        // Skip this changeset
+        return acc;
+      }
+      // Get the number of added images from the changeset
+      const changesetImages = parseInt(cur.properties.metadata["add-image"]);
+      // Add the number of added images to the total
+      return acc + changesetImages;
+    }, 0);
+
+    // Total number of added points of all changesets
+    const points = mapCompleteChangesets.reduce((acc, cur) => {
+      // Check if the changeset has the create metadata
+      if (cur.properties.metadata["create"] === undefined) {
+        // Skip this changeset
+        return acc;
+      }
+      // Get the number of added points from the changeset
+      const changesetPoints = parseInt(cur.properties.metadata["create"]);
+      // Add the number of added points to the total
+      return acc + changesetPoints;
+    }, 0);
+
+    let lastId: number | null;
+    let lastUser: string | null;
+    let lastTheme: string | null;
+    let lastColor: string;
+    let lastColorRgb: [number, number, number] | null;
+
+    // Check if we actually have any changesets before we try to get the last changeset
+    if (mapCompleteChangesets.length > 0) {
+      // Get the last changeset
+      const lastChangeset =
+        mapCompleteChangesets[mapCompleteChangesets.length - 1];
+      // Get the last changeset ID
+      lastId = lastChangeset.id;
+      // Get the last changeset user
+      lastUser = lastChangeset.properties.user;
+      // Get the last changeset theme
+      lastTheme = lastChangeset.properties.metadata["theme"];
+      // Get the last changeset color
+      lastColor = colors[colors.length - 1];
+      lastColorRgb = hexToRgb(lastColor);
+    } else {
+      // Set the last changeset ID to null
+      lastId = null;
+      // Set the last changeset user to null
+      lastUser = null;
+      // Set the last changeset theme to null
+      lastTheme = null;
+      // Set the last changeset color to null
+      lastColor = null;
+      lastColorRgb = null;
+    }
+
+    const statistics = {
+      changesets: {
+        total,
+        last: lastId,
+        lastColor,
+        lastColorRgb,
+        colors,
+        colorsStr: colors.join(","),
+        colorsRgb: colors.map((c) => hexToRgb(c)),
+        colorsRgbStr: colors.map((c) => hexToRgb(c)).join(","),
+      },
+      users: {
+        total: userCount,
+        top: findTop(users),
+        last: lastUser,
+        users,
+      },
+      themes: {
+        total: themeCount,
+        top: findTop(themes),
+        last: lastTheme,
+        themes,
+      },
+      questions,
+      images,
+      points,
+    };
+
+    // Log the statistics
+    logger.info(
+      `Total changesets for today: ${statistics.changesets.total} by ${statistics.users.total} users, using ${statistics.themes.total} different themes`
+    );
+    logger.info(
+      `Top 5 users: ${Object.entries(statistics.users.users)
+        .slice(0, 5)
+        .map(([user, count]) => `${user} (${count})`)
+        .join(", ")}`
+    );
+    logger.info(
+      `Top 5 themes: ${Object.entries(statistics.themes.themes)
+        .slice(0, 5)
+        .map(([theme, count]) => `${theme} (${count})`)
+        .join(", ")}`
+    );
+    logger.info(
+      `Total questions answered: ${statistics.questions}, total images added: ${statistics.images}, total points added: ${statistics.points}`
+    );
+
+    if (!dry_run) {
+      // Publish the statistics to MQTT
+      logger.info("Publishing statistics to MQTT");
+      await client.publish(
+        "mapcomplete/statistics",
+        JSON.stringify(statistics),
+        {
+          retain: true,
+        }
+      );
+
+      // Also publish everything to its own topic
+      for (const [key, value] of Object.entries(statistics)) {
+        // Handle nested objects
+        if (typeof value === "object") {
+          for (const [subKey, subValue] of Object.entries(value)) {
+            await client.publish(
+              `mapcomplete/statistics/${key}/${subKey}`,
+              JSON.stringify(subValue),
+              {
+                retain: true,
+              }
+            );
+          }
+          // Also publish the whole object
           await client.publish(
-            `mapcomplete/statistics/${key}/${subKey}`,
-            JSON.stringify(subValue),
+            `mapcomplete/statistics/${key}`,
+            JSON.stringify(value),
+            {
+              retain: true,
+            }
+          );
+        } else if (typeof value === "number") {
+          await client.publish(
+            `mapcomplete/statistics/${key}`,
+            value.toString(),
+            {
+              retain: true,
+            }
+          );
+        } else if (typeof value === "string") {
+          await client.publish(`mapcomplete/statistics/${key}`, value, {
+            retain: true,
+          });
+        } else {
+          await client.publish(
+            `mapcomplete/statistics/${key}`,
+            JSON.stringify(value),
             {
               retain: true,
             }
           );
         }
-        // Also publish the whole object
-        await client.publish(
-          `mapcomplete/statistics/${key}`,
-          JSON.stringify(value),
-          {
-            retain: true,
-          }
-        );
-      } else if (typeof value === "number") {
-        await client.publish(
-          `mapcomplete/statistics/${key}`,
-          value.toString(),
-          {
-            retain: true,
-          }
-        );
-      } else if (typeof value === "string") {
-        await client.publish(`mapcomplete/statistics/${key}`, value, {
-          retain: true,
-        });
-      } else {
-        await client.publish(
-          `mapcomplete/statistics/${key}`,
-          JSON.stringify(value),
-          {
-            retain: true,
-          }
-        );
       }
+    } else {
+      logger.info("Dry run, not publishing statistics to MQTT", statistics);
     }
-  } else {
-    console.log("Dry run, not publishing statistics to MQTT", statistics);
-  }
 
-  // Update the lastChangesetTime
-  lastUpdateTime = new Date().getTime();
+    // Update the lastChangesetTime
+    lastUpdateTime = new Date().getTime();
+  } catch (error) {
+    logger.error("Error while updating statistics", error);
+  }
 }
 
 main();
